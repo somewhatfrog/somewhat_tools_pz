dff1cc4470ea8cf40f45c0acd3da0b991b1f2501
somewhatfrog <62570891+somewhatfrog@users.noreply.github.com>
2025-10-20 18:01:24 +0300

    Update swt_pz_change_encoding.py
    added support for different block types

--- swt_pz_change_encoding.py
+++ swt_pz_change_encoding.py
@@ -8,67 +8,70 @@ while True:
     else:
         print("No such dir")
 
+# ask for translation type
+block_type = input("Block Type: ").strip()
+
 # ask for build version
 build = input("Build: ").strip()
 
 # encoding maps
 encoding_map_41 = {
-    "UI_AR.txt": "cp1252",
-    "UI_CA.txt": "iso-8859-15",
-    "UI_CH.txt": "utf-8",
-    "UI_CN.txt": "utf-8",
-    "UI_CS.txt": "cp1250",
-    "UI_DA.txt": "cp1252",
-    "UI_DE.txt": "cp1252",
-    "UI_EN.txt": "utf-8",
-    "UI_ES.txt": "cp1252",
-    "UI_FI.txt": "cp1252",
-    "UI_FR.txt": "cp1252",
-    "UI_HU.txt": "cp1250",
-    "UI_ID.txt": "utf-8",
-    "UI_IT.txt": "cp1252",
-    "UI_JP.txt": "utf-8",
-    "UI_KO.txt": "utf-16",
-    "UI_NL.txt": "cp1252",
-    "UI_NO.txt": "cp1252",
-    "UI_PH.txt": "utf-8",
-    "UI_PL.txt": "cp1250",
-    "UI_PT.txt": "cp1252",
-    "UI_PTBR.txt": "cp1252",
-    "UI_RO.txt": "utf-8",
-    "UI_RU.txt": "cp1251",
-    "UI_TH.txt": "utf-8",
-    "UI_TR.txt": "cp1254",
-    "UI_UA.txt": "cp1251"
+    "_AR.txt": "cp1252",
+    "_CA.txt": "iso-8859-15",
+    "_CH.txt": "utf-8",
+    "_CN.txt": "utf-8",
+    "_CS.txt": "cp1250",
+    "_DA.txt": "cp1252",
+    "_DE.txt": "cp1252",
+    "_EN.txt": "utf-8",
+    "_ES.txt": "cp1252",
+    "_FI.txt": "cp1252",
+    "_FR.txt": "cp1252",
+    "_HU.txt": "cp1250",
+    "_ID.txt": "utf-8",
+    "_IT.txt": "cp1252",
+    "_JP.txt": "utf-8",
+    "_KO.txt": "utf-16",
+    "_NL.txt": "cp1252",
+    "_NO.txt": "cp1252",
+    "_PH.txt": "utf-8",
+    "_PL.txt": "cp1250",
+    "_PT.txt": "cp1252",
+    "_PTBR.txt": "cp1252",
+    "_RO.txt": "utf-8",
+    "_RU.txt": "cp1251",
+    "_TH.txt": "utf-8",
+    "_TR.txt": "cp1254",
+    "_UA.txt": "cp1251"
 }
 encoding_map_42 = {
-    "UI_AR.txt": "cp1252",
-    "UI_CA.txt": "iso-8859-15",
-    "UI_CH.txt": "utf-8",
-    "UI_CN.txt": "utf-8",
-    "UI_CS.txt": "cp1250",
-    "UI_DA.txt": "utf-8",
-    "UI_DE.txt": "utf-8",
-    "UI_EN.txt": "utf-8",
-    "UI_ES.txt": "utf-8",
-    "UI_FI.txt": "utf-8",
-    "UI_FR.txt": "utf-8",
-    "UI_HU.txt": "utf-8",
-    "UI_ID.txt": "utf-8",
-    "UI_IT.txt": "utf-8",
-    "UI_JP.txt": "utf-8",
-    "UI_KO.txt": "utf-16",
-    "UI_NL.txt": "utf-8",
-    "UI_NO.txt": "utf-8",
-    "UI_PH.txt": "utf-8",
-    "UI_PL.txt": "utf-8",
-    "UI_PT.txt": "utf-8",
-    "UI_PTBR.txt": "utf-8",
-    "UI_RO.txt": "utf-8",
-    "UI_RU.txt": "utf-8",
-    "UI_TH.txt": "utf-8",
-    "UI_TR.txt": "utf-8",
-    "UI_UA.txt": "utf-8"
+    "_AR.txt": "cp1252",
+    "_CA.txt": "iso-8859-15",
+    "_CH.txt": "utf-8",
+    "_CN.txt": "utf-8",
+    "_CS.txt": "cp1250",
+    "_DA.txt": "utf-8",
+    "_DE.txt": "utf-8",
+    "_EN.txt": "utf-8",
+    "_ES.txt": "utf-8",
+    "_FI.txt": "utf-8",
+    "_FR.txt": "utf-8",
+    "_HU.txt": "utf-8",
+    "_ID.txt": "utf-8",
+    "_IT.txt": "utf-8",
+    "_JP.txt": "utf-8",
+    "_KO.txt": "utf-16",
+    "_NL.txt": "utf-8",
+    "_NO.txt": "utf-8",
+    "_PH.txt": "utf-8",
+    "_PL.txt": "utf-8",
+    "_PT.txt": "utf-8",
+    "_PTBR.txt": "utf-8",
+    "_RO.txt": "utf-8",
+    "_RU.txt": "utf-8",
+    "_TH.txt": "utf-8",
+    "_TR.txt": "utf-8",
+    "_UA.txt": "utf-8"
 }
 
 if build == "41":
@@ -78,6 +81,12 @@ elif build == "42":
 else:
     print("Unknown build version")
     exit()
+
+# Modify the encoding map to include block type prefix
+modified_encoding_map = {}
+for key, value in encoding_map.items():
+    modified_key = f"{block_type}{key}"
+    modified_encoding_map[modified_key] = value
 
 def convert_encoding(file_path, target_encoding):
     try:
@@ -102,7 +111,7 @@ def convert_encoding(file_path, target_encoding):
         print(f"Error {file_path}: {e}")
 
 # process all matching files in the specified directory and subdirectories
-for filename, target_encoding in encoding_map.items():
+for filename, target_encoding in modified_encoding_map.items():
     # find all files with this name in the specified directory and subdirectories
     for root, dirs, files in os.walk(directory):
         if filename in files:
